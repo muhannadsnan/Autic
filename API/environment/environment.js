a37b3f252f6352627d1a005468176dbc55028ec1
muhannadsnan <msn-23@live.com>
2018-09-18 13:18:29 +0200

    edited connection string in env.db.url

--- API/environment/environment.js
+++ API/environment/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-      uri: 'mongodb://test-mnd:VtgKowpbvz1s0m7nwpxERN0Zvsag0MELNSis12UxrVgtMPlUuYHORuNffI1WfXJCSyfQAMRecr99CDq9IEvsNA==@test-mnd.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
+      uri: 'mongodb://autic-db:UVwLK3Eo3IdUTmqiVbc2u2vlg9muU2ReoMNTJt5JgJAs6q5quecJnPwzg108PuZyWoTXWn57k8AN0KHfO1YL6Q==@autic-db.documents.azure.com:10255/?replicaSet=globaldb',
       uri2: 'mongodb://localhost/autic',
     }
 };
