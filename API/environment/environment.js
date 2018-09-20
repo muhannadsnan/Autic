d60613c2250920209d24b67b4a180fdfab83e02e
muhannadsnan <msn-23@live.com>
2018-09-20 14:31:01 +0200

    refresh env

--- API/environment/environment.js
+++ API/environment/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-      uri: 'mongodb://autic-db:UVwLK3Eo3IdUTmqiVbc2u2vlg9muU2ReoMNTJt5JgJAs6q5quecJnPwzg108PuZyWoTXWn57k8AN0KHfO1YL6Q==@autic-db.documents.azure.com:10255/?ssl=true',
+      uri: 'mongodb://auticdb:1jJc6p88mYNmLbJt0XWEOYT1L0GDc6L6MVr5Gy7zyDekZeMZVeVSPLAfM99vEurRH3XPX1nKAnqDfsvvGGTcMw==@auticdb.documents.azure.com:10255/?ssl=true',
       messageConnected: "Connected to Azure DB!",
       port: 3000
     },
