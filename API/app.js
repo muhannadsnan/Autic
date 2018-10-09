26c84a86a65d699867a8a9c1122f6711c1662606
muhannadsnan <msn-23@live.com>
2018-10-09 08:58:18 +0200

    New API changes

--- API/app.js
+++ API/app.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
-var body = require('body-parser');
-var env = require('./environment/environment')
-var envDB = env.db;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // configure the app to use bodyParser()
+//ENV
+var environment = require('./environment/environment')
+global.ENV = environment.env1;
+//CONTROLLERS
 var catsController = require("./controllers/catsController");
-
 // ROUTES
 var routing = require('./routes');
 app.use(routing.cats);
 app.get('/autic', catsController.index);
 app.get('/', (req, resp)=>{
-    resp.send("<h1>Hallooooooo !</h1>");
+    resp.send(`<h1>${ENV.db.messageConnected} on port:<br> ${ENV.port}</h1>`);
 });
-
 // LISTEN
-app.listen(envDB.port);
-console.log(`Node running on port ${envDB.port}...`);
+app.listen(ENV.port, ()=> console.log(`Node running on port ${ENV.port}...`) );
